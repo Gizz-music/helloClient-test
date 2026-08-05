d568f260637b269e646a1269d40594f0844baa65
Vladimir Petrov <poulframan@gmail.com>
2026-08-06 00:59:20 +0300

    Added deploy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/helloClient-test/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
